d51a0e545e2fee425fa6287969b8cace1019ccd1
csg1922 <jennifersumroy@ConnerGsMacBook.home>
2015-10-23 21:09:58 -0400

    Finished tutorial work but problem with https

---
+++ app.js
@@ -0,0 +1,14 @@
+var router = require("./router.js");
+//Problem:We need a simple way to look at a user's badge count and JS point from a web browser
+//Solution: use Node.js to perform the profile look ups and server our template via HTTPS
+
+//Create a web server
+
+var http = require('http');
+http.createServer(function (request, response){
+	router.home(request, response);
+	router.user(request, response);
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+
+
---
+++ renderer.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+
+function mergeValues(values, content) {
+	//cycle over the keys
+	for(var key in values){
+		//replace all {{key}} witht the value from the values object
+		content = content.replace("{{" + key + "}}", values[key]);
+	}
+
+	//return merged content
+	return content;
+
+}
+
+function view(templateName, values, response) {
+	//Read from the template file
+	var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
+	//insert values into the content
+	fileContents = mergeValues(values, fileContents);
+
+	//write out the contents to the response
+	response.write(fileContents);
+}
+
+module.exports.view = view;
